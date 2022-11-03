cbec3864cdbed0bb9736c34cfe264e82e047cf30
arunthomasalex <arunthomasalex@gmail.com>
2022-11-03 14:34:06 +0530

    Handle file not found exception while starting the service

--- app/terminator.py
+++ app/terminator.py
@@ -5,11 +5,12 @@ import requests
 
 def terminate(monitor_file, shutdown_time):
     while True:
-        modified_time = os.path.getmtime(monitor_file)
-        seconds = int(time.time() - modified_time)
-        hours = seconds / (60 * 60)
-        if hours >= shutdown_time:
-            metadata = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document").json()
-            ec2 = boto3.client('ec2', region_name=metadata["region"])
-            ec2.stop_instances(InstanceIds=[metadata["instanceId"]])
+        if os.path.exists(monitor_file):
+            modified_time = os.path.getmtime(monitor_file)
+            seconds = int(time.time() - modified_time)
+            hours = seconds / (60 * 60)
+            if hours >= shutdown_time:
+                metadata = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document").json()
+                ec2 = boto3.client('ec2', region_name=metadata["region"])
+                ec2.stop_instances(InstanceIds=[metadata["instanceId"]])
         time.sleep(10)
